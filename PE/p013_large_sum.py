0aee65ddf86dc718aee2196fb484d20c555aa851
Lena Erickson <lenacerickson@gmail.com>
2021-09-19 16:59:23 -0500

    Update p013_large_sum.py

--- PE/p013_large_sum.py
+++ PE/p013_large_sum.py
@@ -3,7 +3,6 @@ Work out the first ten digits of the sum of one-hundred 50-digit numbers.
 """
 import io
 
-#perform io on txt file to create a list of 50-digit numbers
 def generateList(filepath: str):
     nums = []
     f = open(filepath)
@@ -14,10 +13,8 @@ def generateList(filepath: str):
         nums.append(int(n))
         f.read(1)
     f.close()
-    # print(nums)
     return nums
 
-#function to sum... is it possible to look at just the first 10 digits and infer whether it will round up?
 def firstTenDigits(nums: list):
     sum = 0
     for i in range(0, len(nums)):
